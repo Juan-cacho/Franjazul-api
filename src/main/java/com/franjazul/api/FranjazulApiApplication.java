c893be0c4bc9ffaa26e08289541a874d01f1baa8
Juan Restrepo C <juan_restrepo23202@elpoli.edu.co>
2025-10-25 21:30:09 -0500

    Primer commit, Se inicia el api

---
+++ src/main/java/com/franjazul/api/FranjazulApiApplication.java
@@ -0,0 +1,13 @@
+package com.franjazul.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranjazulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranjazulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/franjazul/api/FranjazulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franjazul.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranjazulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
